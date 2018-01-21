4e86190b6268b60a738dfddfd3d6678b3b0e77f7
Aakash06 <aakash.jain@knoldus.in>
2018-01-21 16:57:50 +0530

    Remove video

--- index.php
+++ index.php
@@ -2,18 +2,13 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <link rel="icon" href="images/icon.png">
-    <title>Eduspace</title>
+  <script type="text/javascript">
+    window.location = "index2.php";
+</script>
 </head>
 
 
-<frameset rows='100%,*' border='0' framespacing='0' frameborder='NO'>
-    <frame src='introtop.html' name='introtop' scrolling='yes' noresize>
-    <frame src='introbottom.html' name='introbottom' scrolling='yes' noresize>
-</frameset>
-<noframes>This page requires frames. Please upgrade your browser to the latest version.
-    <body bgcolor=#000000>
+    <body>
     </body>
 </noframes>
 </html>
